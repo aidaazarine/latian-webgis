0e08640fb1094880d6abe36a66e6c060873ea383
aidaazarine <76052637+aidaazarine@users.noreply.github.com>
2020-12-15 23:09:58 +0700

    Add files via upload

---
+++ data/pwttitik_1.js
@@ -0,0 +1,8 @@
+var json_pwttitik_1 = {
+"type": "FeatureCollection",
+"name": "pwttitik_1",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "id": "1" }, "geometry": { "type": "Point", "coordinates": [ 109.235866234540609, -7.401355243645478 ] } }
+]
+}
